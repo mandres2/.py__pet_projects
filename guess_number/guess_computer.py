37ebb5a5f9d96a84fe939bb43688825026082da5
Micah Andres <limbo.linux.dev@gmail.com>
2021-05-03 13:48:47 -1000

    added guess computer.py

--- guess_number/guess_computer.py
+++ guess_number/guess_computer.py
@@ -2,19 +2,7 @@ import random
 
 # In this file, the computer will be the one to guess the selected number the user defines
 
-def guess(x):
-    random_number = random.randint(1, x)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess < random_number:
-            print('Sorry, try again. The value was too low')
-        elif guess > random_number:
-            print('Sorry, try again. The value was too high')
-
-    print(f'Congratulations! You have guessed the number {random_number} correctly!')
-
-    def computer_guess(x):
+def computer_guess(x):
         low = 1
         high = x
         feedback = ''
@@ -32,4 +20,4 @@ def guess(x):
 
         print(f'Awesome! The computer has guessed the number {guess} correctly!')
 
-guess(10)
+computer_guess(1000)
