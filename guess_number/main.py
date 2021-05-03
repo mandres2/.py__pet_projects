c6526df80af8d8a5f3242a6258e9089169ab0070
Micah Andres <limbo.linux.dev@gmail.com>
2021-05-03 13:24:23 -1000

    added main.py guess number functionality

--- guess_number/main.py
+++ guess_number/main.py
@@ -5,7 +5,12 @@ def guess(x):
     guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
-        print(guess)
-
+        if guess < random_number:
+            print('Sorry, try again. The value you guessed was low')
+        elif guess > random_number:
+                print('Sorry, try again. The value you guessed was too high')
+    print(f'Congratulations! You have guessed the number {random_number} correctly!')
+# The function where x is equal to the limit
+# Output: Guess a number between 1 and x
 guess(10)
 
